febb72be6899e8af436d0444958c5bba6688ffa5
janChorny <janchorny@tut.by>
2023-03-14 22:00:08 +0300

    feat: add test component and test data

---
+++ my-react-app/src/components/product.tsx
@@ -0,0 +1,35 @@
+import './product.css';
+import { ProductModel } from 'models';
+import React from 'react';
+
+interface ProductProps {
+  product: ProductModel;
+}
+
+export function Product(props: ProductProps) {
+  const [details, setDetails] = React.useState(false);
+  const btnBgClassName = details ? 'product-button-blue' : 'product-button-yellow';
+  const btnClasses = ['product-button', btnBgClassName];
+
+  return (
+    <div className="product-container">
+      <img src={props.product.image} alt={props.product.title} className="product-image" />
+      <p>{props.product.title}</p>
+      <span className="product-price">{props.product.price}</span>
+      <button className={btnClasses.join(' ')} onClick={() => setDetails((prev) => !prev)}>
+        {details ? 'Hide details' : 'Show details'}
+      </button>
+      {details && (
+        <div>
+          <p>{props.product.description}</p>
+          <p>
+            Rate:{' '}
+            <span style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+              {props.product.rating.rate}
+            </span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ my-react-app/src/models.ts
@@ -0,0 +1,14 @@
+export interface ProductModel {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: RatingModel;
+}
+
+export interface RatingModel {
+  rate: number;
+  count: number;
+}

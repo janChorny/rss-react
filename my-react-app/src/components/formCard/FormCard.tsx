22f96b2d86d0207587699b03e3cc72e0b9433459
janChorny <janchorny@tut.by>
2023-03-26 21:30:37 +0300

    feat: add min styles to form card

--- my-react-app/src/components/formCard/FormCard.tsx
+++ my-react-app/src/components/formCard/FormCard.tsx
@@ -1,5 +1,6 @@
 import { FormCardInput } from 'models/models';
 import React, { Component } from 'react';
+import './FormCard.css';
 
 export class FormCard extends Component<FormCardInput> {
   constructor(props: FormCardInput) {
@@ -10,19 +11,21 @@ export class FormCard extends Component<FormCardInput> {
     const { title, date, country, pack, delivery, transfer, pay, profilePicture } = this.props.card;
 
     return (
-      <div>
-        <div>
-          <h3>{title}</h3>
-          <div>{date}</div>
-          <div>{country}</div>
-          <div>
-            {pack && <span>pack</span>}
-            {delivery && <span>delivery</span>}
-            {transfer && <span>transfer</span>}
+      <div className="form-card">
+        <div className="form-card__container">
+          <h3 className="form-card__title">Name: {title}</h3>
+          <div className="form-card__date">Date: {date}</div>
+          <div className="form-card__country">Country: {country}</div>
+          <div className="form-card__additional">
+            Options:
+            {pack && <span> pack</span>}
+            {delivery && <span> delivery</span>}
+            {transfer && <span> transfer</span>}
           </div>
-          <div>{pay}</div>
-          <div>
+          <div className="form-card__pay">Pay: {pay}</div>
+          <div className="form-card__image-container">
             <img
+              className="form-card__image"
               src={profilePicture}
               alt={profilePicture.slice(
                 profilePicture.lastIndexOf('/') + 1,

65e0ef6f0c06513d37d548b4922b70d96d1af089
janChorny <janchorny@tut.by>
2023-04-01 17:16:38 +0300

    feat: rewrite app component

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,40 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MainPage } from './pages/main/MainPage';
 import { AboutPage } from './pages/about/AboutPage';
 import { NotFoundPage } from './pages/notfound/NotFoundPage';
 import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
-import { GeneralState } from 'models/models';
 import { FormsPage } from './pages/forms/FormsPage';
 
-export class App extends Component<Record<string, never>, GeneralState> {
-  constructor(props: Record<string, never>) {
-    super(props);
+export function App() {
+  const [pageTitle, setPageTitle] = React.useState('');
 
-    this.state = {
-      pageTitle: '',
-    };
-  }
+  const handlePageTitle = React.useCallback((pageTitle: React.SetStateAction<string>) => {
+    setPageTitle(pageTitle);
+  }, []);
 
-  setPageTitle = (pageTitle: string) => {
-    this.setState({ pageTitle });
-  };
-
-  render() {
-    return (
-      <>
-        <Header />
-        <main className="main">
-          <Routes>
-            <Route path="/" element={<MainPage setTitle={this.setPageTitle} />} />
-            <Route path="/about" element={<AboutPage setTitle={this.setPageTitle} />} />
-            <Route path="/forms" element={<FormsPage setTitle={this.setPageTitle} />} />
-            <Route path="*" element={<NotFoundPage setTitle={this.setPageTitle} />} />
-          </Routes>
-        </main>
-        <Footer />
-      </>
-    );
-  }
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<MainPage setTitle={handlePageTitle} />} />
+          <Route path="/about" element={<AboutPage setTitle={handlePageTitle} />} />
+          <Route path="/forms" element={<FormsPage setTitle={handlePageTitle} />} />
+          <Route path="*" element={<NotFoundPage setTitle={handlePageTitle} />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
 }

6588e94832fc2a61db15260ae029b24563a8856e
janChorny <janchorny@tut.by>
2023-04-08 21:58:36 +0300

    feat: add popup logics

--- my-react-app/src/components/card/Card.tsx
+++ my-react-app/src/components/card/Card.tsx
@@ -3,24 +3,55 @@ import './Card.css';
 import { MainCardInput } from 'models/models';
 
 export function Card(props: MainCardInput) {
-  const { name, status, species, type, gender, origin, location, image, episode, url, created } =
-    props.card;
+  const { name, status, species, type, gender, image } = props.card;
+  const [showFullCard, setShowFullCard] = React.useState(false);
+  const [hideFullCard, setHideFullCard] = React.useState(false);
   return (
-    <div className="card">
-      <img className="card-img" src={image} alt={name} />
-      <div className="card-name">{name}</div>
-      {/* <div>{created}</div>
-      <div>{episode}</div>
-      <div>{gender}</div>
-      <div>{image}</div>
-      <div>{location.name}</div>
-      <div>{location.url}</div>
-      <div>{origin.name}</div>
-      <div>{origin.url}</div>
-      <div>{species}</div>
-      <div>{status}</div>
-      <div>{type}</div>
-      <div>{url}</div> */}
-    </div>
+    <>
+      <div
+        className="card"
+        onClick={() => {
+          setShowFullCard(true);
+          setHideFullCard(false);
+        }}
+      >
+        <img className="card-img" src={image} alt={name} />
+        <div className="card-name">{name}</div>
+      </div>
+      {showFullCard && !hideFullCard && (
+        <div className="popup">
+          <div className="popup__body">
+            <div className="popup__content">
+              <div className="popup__container">
+                <div className="popup__description">
+                  <div
+                    className="popup__close"
+                    onClick={() => {
+                      setHideFullCard(true);
+                      setShowFullCard(false);
+                    }}
+                  >
+                    X
+                  </div>
+                  <img className="card-img" src={image} alt={name} />
+                  <div className="card-name">{name}</div>
+                  <div>{gender}</div>
+                  <div>{species}</div>
+                  <div>{status}</div>
+                  <div>{type}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="popup__overlay"
+            onClick={() => {
+              setHideFullCard(true);
+              setShowFullCard(false);
+            }}
+          ></div>
+        </div>
+      )}
+    </>
   );
 }

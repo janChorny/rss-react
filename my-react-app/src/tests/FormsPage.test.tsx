df097d1935103b76a5ea208258628f5209669437
janChorny <janchorny@tut.by>
2023-04-16 20:47:23 +0300

    fix: update tests

--- my-react-app/src/tests/FormsPage.test.tsx
+++ my-react-app/src/tests/FormsPage.test.tsx
@@ -3,13 +3,18 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { fn } from 'jest-mock';
 import { FormsPage } from '../pages/forms/FormsPage';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 
 describe('Forms page', () => {
   test('renders forms page', () => {
     const setTitle = fn();
-    const title = render(<FormsPage setTitle={setTitle} />);
+    const title = render(
+      <Provider store={store}>
+        <FormsPage setTitle={setTitle} />
+      </Provider>
+    );
     expect(title).toBeTruthy();
-
     const h1 = title.container.querySelector('h1');
     expect(h1?.textContent).toMatch(/forms/i);
   });

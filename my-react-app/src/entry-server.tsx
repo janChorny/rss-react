23ca51f380567467662ac8623ec99c5530d3d160
janChorny <janchorny@tut.by>
2023-04-23 22:24:50 +0300

    feat: update server and add redner to pipeable stream

--- my-react-app/src/entry-server.tsx
+++ my-react-app/src/entry-server.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from './App';
+import { Provider } from 'react-redux';
+import { StaticRouter } from 'react-router-dom/server';
+import { store } from './store/store';
 
-export function render() {
-  const html = ReactDOMServer.renderToString(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+export function render(url: string, options: ReactDOMServer.RenderToPipeableStreamOptions) {
+  return ReactDOMServer.renderToPipeableStream(
+    <Provider store={store}>
+      <StaticRouter location={url}>
+        <App />
+      </StaticRouter>
+    </Provider>,
+    options
   );
-  return { html };
 }

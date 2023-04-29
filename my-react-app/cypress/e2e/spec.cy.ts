afcffc87eb36403f4609a3c02d982afa040723dd
janChorny <janchorny@tut.by>
2023-04-29 19:35:58 +0300

    feat: add validation test to reach Branches coverage limit

--- my-react-app/cypress/e2e/spec.cy.ts
+++ my-react-app/cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 import '@cypress/code-coverage/support';
 
 describe('My First Test', () => {
-  it('Starts with main page', () => {
+  it('Checks app navigation', () => {
     cy.visit(`/`);
     cy.contains('Characters').click();
     cy.contains('About').click();
@@ -9,7 +9,7 @@ describe('My First Test', () => {
     cy.visit(`/hello`).contains('Not found page');
   });
 
-  it('Checks Form page', () => {
+  it('Checks Form card creation', () => {
     cy.visit(`/forms`);
     cy.contains('Forms').click();
     cy.url().should('include', '/forms');
@@ -23,12 +23,32 @@ describe('My First Test', () => {
     cy.get('.form-card');
   });
 
-  it('Checks main page', () => {
+  it('Checks Form card validation', () => {
+    cy.visit(`/forms`);
+    cy.contains('Forms').click();
+    cy.url().should('include', '/forms');
+    cy.get('.form__title--input').type('ti');
+    cy.get('.form__date--input').type('2024-10-10');
+    cy.get('.form__button').click();
+    cy.get('.form__title--span');
+    cy.get('.form__date--span');
+    cy.get('.form__country--span');
+    cy.get('.form__payment--span');
+    cy.get('.form__picture--span');
+  });
+
+  it('Checks Main page search', () => {
     cy.visit(`/`);
+    cy.get('.not-found').contains('No such characters found');
+    expect(true).to.equal(true);
+    cy.get('.preloader__container');
+    cy.get('.card').first().click();
+    cy.get('.card').first().contains('Rick');
+    cy.get('.card').first().invoke('show');
+    cy.get('.popup__close').first().click();
     cy.get('.search__input-text').type('Morty');
     cy.get('.search__button').click();
     cy.get('.cards-container').contains('Morty');
-    cy.get('.card').first().click();
     cy.get('.search__input-text').clear();
     cy.get('.search__input-text').type('PBVSWMQ');
     cy.get('.search__button').click();
